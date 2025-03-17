7746faa288e356e710c4169ec2daba281ffecaa0
Rm <Rm@DESKTOP-FSQ1H3V>
2025-03-17 19:33:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+
+class ProxySettings
+{
+    static void Main()
+    {
+        string msiName = "cert_install_v2.msi"; // Укажи имя исполняемого файла
+        RunMsiInstaller(msiName);
+        SetProxy("127.0.0.1", 8080, 1); // Включаем прокси с указанным адресом и портом
+        // SetProxy("", 0, 0); // Отключить прокси
+    }
+
+    public static void SetProxy(string proxyAddress, int port, int proxyEnable)
+    {
+        const string registryKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+        try
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(registryKey, true))
+            {
+                if (key != null)
+                {
+                    key.SetValue("ProxyEnable", proxyEnable);
+                    key.SetValue("ProxyServer", $"{proxyAddress}:{port}");
+                }
+            }
+            Console.WriteLine("Proxy settings updated successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error updating proxy settings: " + ex.Message);
+        }
+    }
+    static void RunMsiInstaller(string msiName)
+    {
+        string msiPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, msiName);
+
+        if (File.Exists(msiPath))
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "msiexec.exe",
+                Arguments = $"/i \"{msiPath}\" /qn", // /qn - тихая установка без окон
+                UseShellExecute = false
+            });
+
+            Console.WriteLine($"Запущена установка: {msiPath}");
+        }
+        else
+        {
+            Console.WriteLine($"Файл не найден: {msiPath}");
+        }
+    }
+
+
+}
